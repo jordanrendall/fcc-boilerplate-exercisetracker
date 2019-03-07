c0b276e5440081a94b28f14c102cda1067f252ce
jordanrendall <thomasjrendall@gmail.com>
2019-03-07 06:07:33 -0500

    Added support for CSS files

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,42 @@
+//Allows for use of index.html as a template
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+//Allows for extraction of (s)css files for use in the final page
+const MiniCssWebpackPlugin = require("mini-css-extract-plugin");
 
+//Setup of html plugin
 const htmlPlugin = new HtmlWebPackPlugin({
   template: "./src/index.html",
   filename: "./views/index.html"
 });
 
+//Setup of css plugin
+const miniCssPlugin = new MiniCssWebpackPlugin({
+  filename: "style.css",
+  chunkFilename: "[id].css"
+});
+
 module.exports = {
-  entry: "./src/App.js",
+  entry: "./src/index.js",
   module: {
     rules: [
       {
+        //JS & JSX
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
+        loader: "babel-loader"
       },
       {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"]
+        //Sass & css
+        test: /\.scss$/,
+        //Applied from last to first
+        use: [
+          "style-loader",
+          MiniCssWebpackPlugin.loader,
+          "css-loader",
+          "sass-loader"
+        ]
       }
     ]
   },
-  plugins: [htmlPlugin]
+  plugins: [htmlPlugin, miniCssPlugin]
 };
