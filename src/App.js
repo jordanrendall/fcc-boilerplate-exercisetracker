de0d4d8f8f577ac3685c86ee79240f5e2d7ecf5e
jordanrendall <thomasjrendall@gmail.com>
2019-03-07 06:07:54 -0500

    Separated app into app component and index file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import "./App.css";
 
 class App extends React.Component {
   render() {
     return <h1 id="testId">Hello World from React boilerplate</h1>;
   }
 }
-ReactDOM.render(<App />, document.getElementById("root"));
+
+export default App;
