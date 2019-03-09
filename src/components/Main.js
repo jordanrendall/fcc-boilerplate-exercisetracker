3fa8a27405f5272a674f99708d4ceabe966f099c
jordanrendall <thomasjrendall@gmail.com>
2019-03-09 11:29:44 -0500

    Created main component

---
+++ src/components/Main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Main.scss';
+import Forms from './Forms.js';
+
+class Main extends React.Component {
+  render() {
+    return (
+      <main>
+        <Forms />
+      </main>
+    );
+  }
+}
+
+export default Main;
