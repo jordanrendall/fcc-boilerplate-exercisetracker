b8b6583f3a9fc3ad3f7b37d15a1e3f37f6b10163
jordanrendall <thomasjrendall@gmail.com>
2019-03-09 07:06:47 -0500

    Created Forms component

---
+++ src/components/Forms.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Forms.scss';
+import './SubmitForm';
+
+class Forms extends React.Component {
+  render() {
+    return (
+      <div id="forms-container">
+        <SubmitForm />
+      </div>
+    );
+  }
+}
+
+export default Forms;
