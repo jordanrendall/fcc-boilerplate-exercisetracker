a421378fc2daf45135ef5b6fa76edd073a107a19
jordanrendall <thomasjrendall@gmail.com>
2019-03-09 07:06:57 -0500

    Created submission counter component

---
+++ src/components/SubmissionCounter.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './SubmissionCounter.scss';
+
+class SubmissionCounter extends React.Component {
+  render() {
+    return <div id="submission-counter" />;
+  }
+}
+
+export default SubmissionCounter;
