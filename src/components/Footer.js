cc376f0332a858e96926f2401e476dfc7174127c
jordanrendall <thomasjrendall@gmail.com>
2019-03-09 11:29:28 -0500

    Created footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Footer.scss';
+
+class Footer extends React.Component {
+  render() {
+    return <footer id="footer">I am the footer</footer>;
+  }
+}
+
+export default Footer;
